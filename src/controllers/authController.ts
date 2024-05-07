e14bb0b24f94c3fb7cea743842b180bb1cefcb43
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-05-07 16:06:12 +0300

    Tests

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import {Request, Response, Router} from "express";
 import {usersService} from "../services/users-service";
 import {CodeResponsesEnum} from "../utils/utils";
-import {validateAuthRequests, validateErrorsMiddleware} from "../middlewares/middlewares";
+import {authMiddleware, validateAuthRequests, validateErrorsMiddleware} from "../middlewares/middlewares";
 import {jwtService} from "../application/jwt-service";
+import {usersQueryRepository} from "../repositories/query-repositories/users-query-repository";
 
 export const authController = Router({});
 
@@ -15,15 +16,19 @@ authController.post('/login', validateAuthRequests, validateErrorsMiddleware, as
       res.status(CodeResponsesEnum.OK_200).send(token);
 });
 
-authController.get('/me', async (req: Request, res: Response) => {
-    const user = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password);
+authController.get('/me', authMiddleware, async (req: Request, res: Response) => {
+    const myID = req.userId
+    if (!myID){
+        return res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+    }
+    const user = await usersQueryRepository.findUserByID(myID);
     if (!user){
         return res.sendStatus(CodeResponsesEnum.Unauthorized_401)
     }
     res.status(CodeResponsesEnum.OK_200).send({
         email: user.email,
         login: user.login,
-        userId: user._id.toString()
+        userId: myID
     })
 });
 
